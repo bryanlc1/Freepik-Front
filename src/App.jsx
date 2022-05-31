536ffea3ed8851d8d128c7059756831832c1dc32
Bryan Lojan <bryanlc9808@gmail.com>
2022-05-31 10:57:43 +0200

    change import pages in app  and create card player

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import {PlayerProvider} from './context/PlayerContext'
-import Home from './components/Home';
+import Home from './pages/Home';
+import Nav from './pages/Nav'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <PlayerProvider>
+      <Nav/>
       <Home/>
     </PlayerProvider>
   );
