a64b20642798a70c84db2ffc438f3ff5715e582a
Bryan Lojan <bryanlc9808@gmail.com>
2022-05-31 16:09:41 +0200

    constants

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -5,7 +5,18 @@ const playerContext = createContext()
 
 export const PlayerProvider = ({ children }) => {
 	const [player, setPlayer] = useState({})
-	const [myPlayers, setMyPlayers] = useState([])
+	const [myPlayers, setMyPlayers] = useState([
+        {id:1456,
+        country:'Spain',
+        price:180000,
+        position:'Defender',
+        age:25},
+        {id:1275,
+            country:'Ecuador',
+            price:190000,
+            position:'LeftWinger',
+            age:25}
+    ])
 
 	return (
 		<playerContext.Provider value={{ player, setPlayer, myPlayers, setMyPlayers }}>
