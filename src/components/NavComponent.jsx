619d9660560b3bcbf37453f4c0744a8cc2c62199
cesar david moreno <cesardaviddev@gmail.com>
2022-05-31 11:41:02 +0200

    Nav

--- src/components/NavComponent.jsx
+++ src/components/NavComponent.jsx
@@ -3,11 +3,10 @@ import { Navbar } from "react-bootstrap";
 import { Nav } from "react-bootstrap";
 import { Container } from "react-bootstrap";
 
-
 function NavComponent() {
   return (
     <div className="nav">
-      <Navbar bg="dark" variant="dark">
+      <Navbar bg="dark" variant="dark" fixed="top" expand="sm">
         <Container>
           <Navbar.Brand href="#home">
             <i class="fi fi-rr-football freepik"></i> Freepik
